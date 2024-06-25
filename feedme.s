a0b1ac166aebe235aa71685f06f38ef45532be00
VirtualWizard100 <139145827+VirtualWizard100@users.noreply.github.com>
2024-06-25 14:19:43 -0400

    Add files via upload

---
+++ feedme.s
@@ -0,0 +1,24 @@
+.include "toUpper.s"
+.global _start
+
+_start:
+	ldr x3, =0x00656d2064656566
+	ldr x4, =fillblock
+	ldr x5, =lowercase
+	str x3, [x5]
+	toupper lowercase, fillblock
+	orr x5, xzr, x0
+	mov x0, #1
+	ldr x1, =fillblock
+	sub x2, x4, x5
+	mov x8, #0x40
+	svc 0
+exit:
+	mov x0, #0
+	mov x8, #0x5d
+	svc 0
+.data
+lowercase:
+	.fill 30, 4, 0
+fillblock:
+	.fill 30, 4, 0

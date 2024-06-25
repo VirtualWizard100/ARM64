e3f2feb36b7003c3e982ba1021dd272e89d63ba3
VirtualWizard100 <139145827+VirtualWizard100@users.noreply.github.com>
2024-06-25 15:47:42 -0400

    Add files via upload

--- feedme.s
+++ feedme.s
@@ -2,15 +2,18 @@
 .global _start
 
 _start:
-	ldr x3, =0x002e656d2064656566 /*.em deef */
+	ldr x3, =0x2e656d2064656566 /* .em deef */
 	ldr x4, =fillblock
+	mov w9, #0x0a
+	strb w9, [x4, #9]
 	ldr x5, =lowercase
 	str x3, [x5]
 	toupper lowercase, fillblock
 	orr x5, xzr, x0
 	mov x0, #1
 	ldr x1, =fillblock
-	sub x2, x4, x5
+	sub x2, x4, x1
+	add x2, x2, #1
 	mov x8, #0x40
 	svc 0
 exit:
@@ -19,6 +22,6 @@ exit:
 	svc 0
 .data
 lowercase:
-	.fill 30, 4, 0
+	.fill 30, 1, 0
 fillblock:
-	.fill 30, 4, 0
+	.fill 30, 1, 0
